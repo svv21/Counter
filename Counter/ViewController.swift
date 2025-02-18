227e421dd61173d68ead532440804ab6193418f8
Vladislava Scherbo <vladislava.volokhovich@gmail.com>
2025-02-18 22:01:25 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Vladislava Scherbo on 18.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
